22f43fdbda0f4bfd0c50f77ed1fa6a5ccf5fec39
Thizh Wick <70251552+Thizh@users.noreply.github.com>
2023-10-21 14:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ Project Name: Binary Search Algorithm
 Author: Thisitha Wickramarachchi
 
 Description: This program will take any input starting from 0 to any range 
-that you specify and display a range of numbers with a difference of two..
+that you specify and display a range of numbers with a difference of two.
 '''
 
 #take inputs from user
-startPoint = int(input("Add a starting number: "))
-endPoint = int(input("Add a ending number: "))
+while True :
+    startPoint = int(input("Add a starting number: "))
+    if startPoint >= 0 :
+        break
+    else :
+        print("Enter a number bigger than 0")
+        continue
+
+while True:    
+    endPoint = int(input("Add a ending number: "))
+    if endPoint >= 0 :
+        break
+    else :
+        print("Enter a number bigger than 0")
+        continue
 
 #display a range of numbers with a difference of two
 for i in range(startPoint, endPoint, 2) :
